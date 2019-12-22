7cf0e09b65a42b485e131a4544887d0d40812eda
Michael Klein <michael.klein.business@gmail.com>
2019-12-22 12:02:33 +0100

    removed dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Components<Component extends any> {
-    [key: string]: Component;
-}
-export interface HtmdxOptions<Component extends any = any, C extends Components<Component> = Components<Component>> {
-    components?: C;
-}
-export declare function htmdx<H extends (type: string, props: any, ...children: any[]) => any = (type: string, props: any, ...children: any[]) => any, O extends HtmdxOptions = HtmdxOptions>(m: string, h: H, options?: O): ReturnType<H>;
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./htmdx.cjs.production.min.js')
-} else {
-  module.exports = require('./htmdx.cjs.development.js')
-}
