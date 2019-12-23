d363bf50e2fbadc80f9888563398b67203aa8bee
Jason Miller <developit@google.com>
2019-12-23 17:46:01 -0500

    Remove HTML parsing logic in favor of a modifier function that sits between HTM and createElement

--- src/index.ts
+++ src/index.ts
@@ -6,46 +6,22 @@ export interface Components<Component extends any> {
   [key: string]: Component;
 }
 
-function handleComponents(m: string): string {
-  var regexAllTags = /<([A-Z][a-zA-Z1-6]+)([^<]+)*(?:>(.*)<\/\1>|\s+\/>)/g;
-
-  var htmlTags = m.match(regexAllTags);
-  var regexSingleTag = /<([A-Z][a-zA-Z1-6]+)([^<]+)*(?:>(.*)<\/\1>|\s+\/>)/;
-  if (htmlTags)
-    for (var i = 0; i < htmlTags.length && i < 5; i++) {
-      var match = regexSingleTag.exec(htmlTags[i]);
-      if (match) {
-        const newText = match[0]
-          .replace(`<${match[1]}`, '<${' + match[1] + '}')
-          .replace(`</${match[1]}`, '</${' + match[1] + '}');
-        m = m.replace(match[0], newText);
-      }
-    }
-  return m;
-}
-
 function markedToReact<
   Component extends any = any,
   C extends Components<Component> = Components<Component>
 >(
   m: string,
   components: Components<C>,
-  html: () => any,
+  h: jsxFactory,
   thisValue: any = {}
 ): any {
   m = decode(m);
-  m = handleComponents(m);
-  m.split(/<code(.|\n)*<\/code>/g).forEach(str => {
-    m = m.replace(str, str.replace(/class=/g, 'className='));
-  });
+  currentFactory = h;
+  currentComponents = components;
   return new Function(
-    ...[
-      'html',
-      'thisValue',
-      ...Object.keys(components),
-      'return (function() {return html`' + m + '`}).call(thisValue)',
-    ]
-  )(...[html, thisValue, ...Object.values(components)]);
+    'html',
+    'return html`'+m+'`'
+  ).call(thisValue, html);
 }
 export type Marked = typeof marked;
 export interface HtmdxOptions<
@@ -59,22 +35,35 @@ export interface HtmdxOptions<
   thisValue?: ThisValue;
 }
 
+const html = htm.bind(<jsxFactory>function(type, props, ...children) {
+  // remap class prop to className
+  if (props != null && 'class' in props) {
+    props.className = props.class;
+    delete props.class;
+  }
+  // remap uppercase-leading tags to defined components
+  if (typeof type === 'string' && isJsxTag(type)) {
+    type = currentComponents[type];
+  }
+  return currentFactory(type, props, ...children);
+});
+
 function transFormJSXToHTM(m: string): string {
-  m.split(/```(.|\n)*```/g).forEach(str => {
-    m = m.replace(
-      str,
-      str.replace(/class=/g, 'className=').replace(/={/g, '=${')
-    );
-  });
-  return m;
+  // transform JSX expressions to HTM expressions, but not in fenced blocks.
+  return m.replace(/(```+)[\s\S]*?\2|={/g, (str, fence) => fence ? str : '=${');
 }
 
+function isJsxTag(type: string): boolean {
+  return /^[A-Z_$]|\./.test(type);
+}
+
+type jsxFactory = (type: string, props: any, ...children: any[]) => any;
+
+let currentFactory: jsxFactory;
+let currentComponents: Components<any>;
+
 export function htmdx<
-  H extends (type: string, props: any, ...children: any[]) => any = (
-    type: string,
-    props: any,
-    ...children: any[]
-  ) => any,
+  H extends jsxFactory,
   O extends HtmdxOptions = HtmdxOptions
 >(m: string, h: H, options: O = {} as O): ReturnType<H> {
   const {
@@ -88,7 +77,7 @@ export function htmdx<
   return markedToReact(
     marked(transformJSXToHTM ? transFormJSXToHTM(m) : m),
     components,
-    htm.bind(h),
+    h,
     options.thisValue || {}
   );
 }
--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { htmdx } from '../dist';
+import { htmdx } from '../src';
 
 const simpleMarkdown = `
 # Hello World
